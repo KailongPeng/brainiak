1796e1848f242f459f8363623730e6afa4cb9f2d
KailongPeng <kailongpeng001@gmail.com>
2022-08-24 12:00:39 -0400

    comment

--- examples/searchlight/kpTest/example_searchlight.py
+++ examples/searchlight/kpTest/example_searchlight.py
@@ -14,6 +14,7 @@
 
 import numpy as np
 from mpi4py import MPI
+import nibabel as nib
 import sys
 
 from brainiak.searchlight.searchlight import Searchlight
@@ -33,14 +34,15 @@ size = comm.size
 
 # 数据集大小参数  Dataset size parameters
 
-dim = 40
+# dim = 40
 
-# dim1 = 91
-# dim2 = 109
-# dim3 = 91
+dim1 = 91
+dim2 = 109
+dim3 = 91
 
-ntr = 400  # 400   576
-maskrad = 15
+ntr = 576  # 400   576
+kneeNumber = 18
+# maskrad = 15
 
 # 预测点参数  Predictive point parameters
 pt = (23, 23, 23)
@@ -48,19 +50,31 @@ kernel_dim = 5
 weight = 1
 
 # 生成数据  Generate data
-data = np.random.random((dim, dim, dim, ntr)) if rank == 0 else None  # data 只有在rank==0的时候才有值, 其他的时候都是None
-mask = np.zeros((dim, dim, dim), dtype=np.bool)  # mask在任何rank都有值. 不光是在rank==0的时候.
-for i in range(dim):
-    for j in range(dim):
-        for k in range(dim):
-            dist = np.sqrt(((dim / 2) - i) ** 2 + ((dim / 2) - j) ** 2 + ((dim / 2) - k) ** 2)
-            if dist < maskrad:
-                mask[i, j, k] = 1
-print(f"mask.shape={mask.shape}")
-print(f"np.nansum(mask)={np.nansum(mask)}")
+data = np.random.random((dim1, dim2, dim3, ntr)) if rank == 0 else None  # data 只有在rank==0的时候才有值, 其他的时候都是None
+
+subjectDir = "/gpfs/milgram/project/turk-browne/projects/localize/analysis/subjects/"
+subject = 'sub001'
+mask = nib.load(
+    f"{subjectDir}/{subject}/preprocess/allruns.gfeat/mask.nii.gz")  # /gpfs/milgram/project/turk-browne/projects/localize/analysis/subjects/sub004/preprocess/allruns.gfeat/mask.nii.gz  91x109x91 stand space
+affine_mat = mask.affine
+print(f"affine_mat.shape={affine_mat.shape}")
+mask = mask.get_fdata()
+allMaskSum = np.nansum(mask)
+print('number of voxels in mask: {}'.format(allMaskSum))  # 239565
+
+# mask = np.zeros((dim, dim, dim), dtype=np.bool)  # mask在任何rank都有值. 不光是在rank==0的时候.
+# for i in range(dim):
+#     for j in range(dim):
+#         for k in range(dim):
+#             dist = np.sqrt(((dim / 2) - i) ** 2 + ((dim / 2) - j) ** 2 + ((dim / 2) - k) ** 2)
+#             if dist < maskrad:
+#                 mask[i, j, k] = 1
+# print(f"mask.shape={mask.shape}")
+# print(f"np.prod(mask.shape)={np.prod(mask.shape)}")
+# print(f"np.nansum(mask)={np.nansum(mask)}")
 
 # 生成标签  Generate labels
-labels = np.random.choice([True, False], (ntr,)) if rank == 0 else None  # 只有当rank==0的时候labels才有值,其他时候都是None
+labels = np.random.choice([True, False], (ntr, kneeNumber)) if rank == 0 else None  # 只有当rank==0的时候labels才有值,其他时候都是None
 
 # 在随机数据中注入预测性区域  Inject predictive region in random data
 if rank == 0:
@@ -72,7 +86,7 @@ if rank == 0:
                 kernel[i, j, k] = np.exp(-np.dot(arr.T, arr))
     kernel = kernel / np.sum(kernel)
 
-    for (idx, l) in enumerate(labels):
+    for (idx, l) in enumerate(labels[:, 0]):
         if l:
             data[pt[0]:pt[0] + kernel_dim, pt[1]:pt[1] + kernel_dim, pt[2]:pt[2] + kernel_dim, idx] += kernel * weight
         else:
@@ -100,7 +114,7 @@ def sfn(l, msk, myrad, bcast_var):
     data = l[0][msk, :].T
     print(f"data.shape={data.shape}")
     print(f"bcast_var.shape={bcast_var.shape}")
-    return np.mean(sklearn.model_selection.cross_val_score(classifier, data, bcast_var, n_jobs=1))
+    return np.mean(sklearn.model_selection.cross_val_score(classifier, data, bcast_var[:, 0], n_jobs=1))
 
 
 # Run searchlight
@@ -116,3 +130,5 @@ global_outputs = sl.run_searchlight(sfn)
 #         plt.imshow(img, cmap='hot', vmin=0, vmax=1)
 #         plt.savefig('img' + str(cnt) + '.png')
 #         plt.clf()
+
+print("done")
